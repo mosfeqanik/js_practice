298c6bf2151078ebafc8727fcb3247fbbe6cf860
Anik Mosfeq <mosfeqanik01@gmail.com>
2022-04-06 14:58:06 +0600

    var usage

---
+++ index.js
@@ -0,0 +1,12 @@
+var firstNumber = prompt('Put Your Number');
+var secondNumber = 10;
+
+
+console.log(firstNumber);
+console.log(firstNumber);
+
+if(firstNumber%2==0){
+    alert(firstNumber + " is even"); 
+}else{
+    alert(firstNumber + " is odd");
+}
